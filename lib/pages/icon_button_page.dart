5f28bb129081873d52f866829f519e202318b731
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-01-22 19:05:20 +0700

    icon button

--- lib/pages/icon_button_page.dart
+++ lib/pages/icon_button_page.dart
@@ -5,7 +5,8 @@ class IconButtonPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     _showToast() {
-      Fluttertoast.showToast(msg: 'Hello there', toastLength: Toast.LENGTH_SHORT);
+      Fluttertoast.showToast(
+          msg: 'Hello there', toastLength: Toast.LENGTH_SHORT);
     }
 
     return Scaffold(
@@ -13,15 +14,153 @@ class IconButtonPage extends StatelessWidget {
         title: Text('IconButton'),
       ),
       body: Container(
-        child: Row(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            IconButton(
-              iconSize: 64,
-              icon: Icon(
-                Icons.payment,
-                color: Colors.blue,
-              ),
-              onPressed: _showToast,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.payment,
+                    color: Colors.blue,
+                  ),
+                  onPressed: _showToast,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.lock_outline,
+                    color: Colors.orange,
+                  ),
+                  onPressed: _showToast,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.looks,
+                    color: Colors.green,
+                  ),
+                  onPressed: _showToast,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.payment,
+                    color: Colors.blue,
+                  ),
+                  onPressed: _showToast,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.lock_outline,
+                    color: Colors.orange,
+                  ),
+                  onPressed: _showToast,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.looks,
+                    color: Colors.green,
+                  ),
+                  onPressed: _showToast,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.payment,
+                    color: Colors.blue,
+                  ),
+                  onPressed: _showToast,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.lock_outline,
+                    color: Colors.orange,
+                  ),
+                  onPressed: _showToast,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.looks,
+                    color: Colors.green,
+                  ),
+                  onPressed: _showToast,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.payment,
+                    color: Colors.blue,
+                  ),
+                  onPressed: _showToast,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.lock_outline,
+                    color: Colors.orange,
+                  ),
+                  onPressed: _showToast,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.looks,
+                    color: Colors.green,
+                  ),
+                  onPressed: _showToast,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.payment,
+                    color: Colors.blue,
+                  ),
+                  onPressed: _showToast,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.lock_outline,
+                    color: Colors.orange,
+                  ),
+                  onPressed: _showToast,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(
+                    Icons.looks,
+                    color: Colors.green,
+                  ),
+                  onPressed: _showToast,
+                )
+              ],
             ),
           ],
         ),
