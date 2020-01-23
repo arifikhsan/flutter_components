c34593962c021597ccd3d5e6030712f0d8335491
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-01-23 21:40:06 +0700

    fix hello world page

--- lib/pages/hello_world_page.dart
+++ lib/pages/hello_world_page.dart
@@ -3,16 +3,13 @@ import 'package:flutter/material.dart';
 class HelloWorldPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Hello World'),
         ),
       ),
     );
