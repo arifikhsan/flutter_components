02cc42ca299b5702ef97789b09b839e142f71619
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-02-03 12:38:04 +0700

    change raised button to list tile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,102 +13,93 @@ class HomePage extends StatelessWidget {
         child: Center(
           child: ListView(
             children: <Widget>[
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
+              ListTile(
+                title: Text(
                   'Hello World',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.blue),
                 ),
-                onPressed: () {
+                onTap: () {
                   Navigator.pushNamed(context, '/hello_world');
                 },
               ),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
+              ListTile(
+                title: Text(
                   'Buttons',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.blue),
                 ),
-                onPressed: () {
+                onTap: () {
                   Navigator.pushNamed(context, '/button');
                 },
               ),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
+              ListTile(
+                title: Text(
                   'Icon',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.blue),
                 ),
-                onPressed: () {
+                onTap: () {
                   Navigator.pushNamed(context, '/icon');
                 },
               ),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
+              ListTile(
+                title: Text(
                   'Icon Button',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.blue),
                 ),
-                onPressed: () {
+                onTap: () {
                   Navigator.pushNamed(context, '/icon_button');
                 },
               ),
-              RaisedButton(
-                  color: Colors.blue,
-                  child: Text(
-                    'Simple Text',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/text');
-                  }),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
-                  'Card',
-                  style: TextStyle(color: Colors.white),
+              ListTile(
+                title: Text(
+                  'Simple Text',
+                  style: TextStyle(color: Colors.blue),
                 ),
-                onPressed: () {
+                onTap: () {
+                  Navigator.pushNamed(context, '/text');
+                },
+              ),
+              ListTile(
+                title: Text(
+                  'Card',
+                  style: TextStyle(color: Colors.blue),
+                ),
+                onTap: () {
                   Navigator.pushNamed(context, '/card');
                 },
               ),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
+              ListTile(
+                title: Text(
                   'Dropdown Button',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.blue),
                 ),
-                onPressed: () {
+                onTap: () {
                   Navigator.pushNamed(context, '/dropdown_button');
                 },
               ),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
+              ListTile(
+                title: Text(
                   'Text field',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.blue),
                 ),
-                onPressed: () {
+                onTap: () {
                   Navigator.pushNamed(context, '/text_field');
                 },
               ),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
+              ListTile(
+                title: Text(
                   'Stateful Widgets',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.blue),
                 ),
-                onPressed: () {
+                onTap: () {
                   Navigator.pushNamed(context, '/stateful_widgets');
                 },
               ),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
+              ListTile(
+                title: Text(
                   'Form',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.blue),
                 ),
-                onPressed: () {
+                onTap: () {
                   Navigator.pushNamed(context, '/form');
                 },
               )
