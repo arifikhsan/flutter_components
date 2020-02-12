669085d30a04c6891ef0533ae59d39b80f6388e1
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-02-12 19:49:08 +0700

    image

--- lib/pages/widgets/image_page.dart
+++ lib/pages/widgets/image_page.dart
@@ -8,55 +8,61 @@ class ImagePage extends StatelessWidget {
         title: const Text('Image'),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Card(
-              clipBehavior: Clip.hardEdge,
-              margin: const EdgeInsets.all(16),
-              elevation: 8,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              Card(
+                clipBehavior: Clip.hardEdge,
+                margin: const EdgeInsets.all(8),
+                elevation: 8,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                child: Image.asset('assets/images/vegetable.jpg'),
+              ),
+              Card(
+                clipBehavior: Clip.hardEdge,
+                margin: const EdgeInsets.all(8),
+                elevation: 8,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                child: Image.asset('assets/images/apple.jpg'),
+              ),
+              Container(
+                width: double.infinity,
+                child: const Card(
+                  clipBehavior: Clip.hardEdge,
+                  margin: EdgeInsets.all(8),
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16),
+                    ),
+                  ),
+                  child: FlutterLogo(
+                    size: 200,
+                  ),
                 ),
               ),
-              child: Image.asset('assets/images/vegetable.jpg'),
-            ),
-            Card(
-              clipBehavior: Clip.hardEdge,
-              margin: const EdgeInsets.all(16),
-              elevation: 8,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
+              Card(
+                clipBehavior: Clip.hardEdge,
+                margin: const EdgeInsets.all(8),
+                elevation: 8,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
                 ),
+                child: Image.asset('assets/images/leaf.jpg'),
               ),
-              child: Image.asset('assets/images/apple.jpg'),
-            ),
-            Card(
-              clipBehavior: Clip.hardEdge,
-              margin: const EdgeInsets.all(16),
-              elevation: 8,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
-                ),
-              ),
-              child: const FlutterLogo(
-                size: 200,
-              ),
-            ),
-            Card(
-              clipBehavior: Clip.hardEdge,
-              margin: const EdgeInsets.all(16),
-              elevation: 8,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
-                ),
-              ),
-              child: Image.asset('assets/images/leaf.jpg'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
